1414f90ee738a8aa454f2622edb514434760ef9e
Cristian <cristiansr2508@gmail.com>
2022-09-05 20:18:56 -0300

    v1

---
+++ 1.js
@@ -0,0 +1,31 @@
+// PARTE 1
+
+// let primerNum = prompt("Escribe un numero");
+// let segunNum = prompt("Escribe otro");
+// let Num1 = parseInt(primerNum);
+// let Num2 = parseInt(segunNum);
+// let suma = Num1 + Num2;
+// alert(suma);
+
+
+// PARTE 2
+
+// let tempo = prompt("Escribe la temperatura en celsius");
+// let num1 = parseInt(tempo) ;
+// let turning = (num1 * 9/5)+32;
+// alert(turning);
+
+
+
+// PARTE 3
+
+// let primerNum = prompt("Escribe un numero");
+// let Num1 = parseInt(primerNum);
+// let div = Num1 / 10;
+// alert(div);
+
+
+
+// PARTE 4
+let a = [1,2,3];
+alert (typeof a);
